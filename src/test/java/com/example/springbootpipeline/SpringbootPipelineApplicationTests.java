62397d50a103a73e9c173aa63b62837323155a76
vinod12347 <kjvinodraj@gmail.com>
2023-12-22 18:01:22 -0500

    new file

---
+++ src/test/java/com/example/springbootpipeline/SpringbootPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
