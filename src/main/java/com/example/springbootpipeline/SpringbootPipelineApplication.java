920294c70922a33385e47a8a4a33bfca76657ae1
vinod12347 <kjvinodraj@gmail.com>
2024-02-23 15:31:41 -0500

    new code changes here

---
+++ src/main/java/com/example/springbootpipeline/SpringbootPipelineApplication.java
@@ -0,0 +1,27 @@
+package com.example.springbootpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/v1")
+public class SpringbootPipelineApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootPipelineApplication.class, args);
+    }
+
+    @GetMapping("/greeting")
+    public ResponseEntity<String> getMessage(){
+        return ResponseEntity.status(HttpStatus.OK).body("User Logged in Successfully");
+    }
+
+}
